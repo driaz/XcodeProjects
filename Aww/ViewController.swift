8fb68f26e52155c86d52f688a778fa8460cc696e
daniel riazgit <daniel.riaz@gmail.com>
2014-11-16 22:28:09 -0800

    Initial Commit

---
+++ Aww/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aww
+//
+//  Created by Daniel Riaz on 11/16/14.
+//  Copyright (c) 2014 Daniel Riaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
